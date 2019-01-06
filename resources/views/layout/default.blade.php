3f56f95f014cc837ff7ba85ea3812ee16810cf4f
aon <251109226@qq.com>
2019-01-06 08:23:44 +0000

    change

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield("title","web app")</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-@yield("content")
+
+@include("layout._header")
+
+<div class="container">
+    @yield('content')
+    @include("layout._footer")
+</div>
 </body>
 </html>
